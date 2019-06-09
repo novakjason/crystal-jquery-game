a981fa6a7c451503d0fbc11eca87ce4239254ca4
Red Tail <redtail814@gmail.com>
2019-06-08 23:04:51 -0400

    Cleaned up javascript comments.  Added more DOM manipulation including button to start next round.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,23 @@
-// Runs when page is loaded.  On-click event buttons.
-
-window.onload = function() {
-  $("#computer-score").html('<div>' + compScore + '</div>');
+// Runs new game when page is loaded and sets on-click event buttons.
+window.onload = function () {
+  newGame();
   $("#crystal-green").on("click", crystalGreen);
   $("#crystal-orange").on("click", crystalOrange);
   $("#crystal-purple").on("click", crystalPurple);
   $("#crystal-red").on("click", crystalRed);
   $("#new-game").on("click", newGame);
-  // $("#hide-me").on("click", hideHowTo);
+  $("#next-round").on("click", nextRound);
+  // $("#hide-me").on("click", hideHowTo);  Would like to add a button to toggle view of instructions.
 };
 
-var losses = 0;
+
+//  Variables to hold value of wins, losses, and user's score.
 var wins = 0;
+var losses = 0;
+var userScore = 0;
 
 
-//    * Crystal is made into an object with nested objects showing name and value.
+//  Crystal is made into an object with nested "color" objects holding a name and value.
 var crystal = {
   green: {
     name: "green",
@@ -34,16 +37,19 @@ var crystal = {
   }
 }
 
+
 //    Function to generate random number for computer score.
-var randomNumber = function(min, max) {
+var randomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-//    Variable to hold computer score.
-compScore = randomNumber(19, 120);
-console.log(compScore);
 
 
-//     Crystals are given hidden values. 
+//    Variable to hold winning number which should be between 19 - 120.
+winningNumber = randomNumber(19, 120);
+console.log(winningNumber);
+
+
+//     Crystals are given hidden values between 1 - 12.
 crystal.green.value = randomNumber(1, 12);
 crystal.orange.value = randomNumber(1, 12);
 crystal.purple.value = randomNumber(1, 12);
@@ -52,10 +58,6 @@ console.log("Green: " + crystal.green.value);
 console.log("Orange: " + crystal.orange.value);
 console.log("Purple: " + crystal.purple.value);
 console.log("Red: " + crystal.red.value);
-
-
-//    Players score starts at 0
-var userScore = 0;
 
 
 //    When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
@@ -90,43 +92,54 @@ function crystalRed() {
 
 //    Function for next round.
 function nextRound() {
-  compScore = randomNumber(19, 120);
-  $("#computer-score").html('<div>' + compScore + '</div>');
+  // Winning number and user's score resets.
+  winningNumber = randomNumber(19, 120);
+  $("#winning-number").html('<div>' + winningNumber + '</div>');
   userScore = 0;
   $("#user-score").text("0");
-  console.log(compScore);
+  console.log(winningNumber);
+  //  Value of each crystal resets.
+  crystal.green.value = randomNumber(1, 12);
+  crystal.orange.value = randomNumber(1, 12);
+  crystal.purple.value = randomNumber(1, 12);
+  crystal.red.value = randomNumber(1, 12);
+  console.log("Green: " + crystal.green.value);
+  console.log("Orange: " + crystal.orange.value);
+  console.log("Purple: " + crystal.purple.value);
+  console.log("Red: " + crystal.red.value);
+  //  Next round button is cleared from DOM.
+  $("#next-round").empty();
+  confetti.stop();
+
 }
 
-//    * The player wins if their total score matches the random number from the beginning of the game.
+//  Function to check user's score against the winning number.
 function checkScore() {
-  if (userScore === compScore) {
+  if (userScore === winningNumber) {
+    confetti.start();
+    //  Updates wins.
     wins++;
-    nextRound();
     $("#wins").html('<div>Wins: ' + wins + '</div>');
+    //  Adds a button to DOM to start next round
+    $("#next-round").html('<div class="btn btn-primary d-inline-flex justify-content-center">You win! Click here to start next round.</div>');
   }
-  else if (userScore > compScore) {
+  //    * The player loses if their score goes above the random number.
+  else if (userScore > winningNumber) {
+    //  Updates losses.
     losses++;
-    nextRound();
     $("#losses").html('<div>Losses: ' + losses + '</div>');
+    //  Adds a button to DOM to start next round.
+    $("#next-round").html('<div class="btn btn-primary d-inline-flex justify-content-center">Sorry you lose! Click here to start next round.</div>');
   }
-
 }
-//    * The player loses if their score goes above the random number.
 
-//    * The game restarts whenever the player wins or loses.
-
-//      * When the game begins again, the player should see a new random number. 
-
-
-//     Of course, the user's score (and score counter) will reset to zero.
 
 //    * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 function newGame() {
   console.log("NEW GAME CLICKED!");
   nextRound();
+  wins = 0;
+  $("#wins").html('<div>Wins: ' + wins + '</div>');
+  losses = 0;
+  $("#losses").html('<div>Losses: ' + losses + '</div>');
 }
-// ##### Option 1 Game design notes
-
-// * The random number shown at the start of the game should be between 19 - 120.
-
-// * Each crystal should have a random hidden value between 1 - 12.
